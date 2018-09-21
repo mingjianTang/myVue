9daa4c2d8ab2724fce05c06837f0e842ff98a03c
mingjianTang <635863075@qq.com>
2018-09-21 15:37:18 +0800

    first time

---
+++ build/vue-loader.config.js
@@ -0,0 +1,11 @@
+module.exports = (isDev) => {
+    return {
+        preserveWhitepace: true,   //对空格的处理
+        extractCSS: !isDev,         //将.vue的css另外打包
+        cssModules: {
+            localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+            camelCase: true
+        },
+        //  hotReload: false,       //根据环境变量生成
+    }
+}
